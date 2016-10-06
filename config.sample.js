c50af06ba13e31973bd0e44016182c38a20b1c71
sapphire <sapphiredev@users.noreply.github.com>
2016-10-06 20:30:30 +0900

    build fix

--- config.sample.js
+++ config.sample.js
@@ -1,9 +1,9 @@
 module.exports = {
 	twitter: {
-		consumer_key: '',
-		consumer_secret: '',
-		access_token: '',
-		access_token_secret: ''
+		consumer_key: 'consumer_key',
+		consumer_secret: 'consumer_secret',
+		access_token: 'access_token',
+		access_token_secret: 'access_token_secret'
 	},
 	knex: {
 		client: 'sqlite3',
