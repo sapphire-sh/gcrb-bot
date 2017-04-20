469c51de410bc4677aa1d67ff561ff4612f42482
sapphire <sapphiredev@users.noreply.github.com>
2017-04-20 06:01:05 -0700

    add ua

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -50,7 +50,12 @@ class Parser {
 		const url = self.composeURL(data);
 
 		return new Promise((resolve, reject) => {
-			request(url, (err, res, body) => {
+			request({
+				url: url,
+				headers: {
+					'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+				}
+			}, (err, res, body) => {
 				let items = [];
 
 				if(!err && res.statusCode === 200) {
