22fee6185d6a02110f667e461ebbd77a5f96525e
sapphire <info@sapphire.sh>
2019-09-28 00:19:21 +0900

    update date string helper

--- src/helpers/__tests__/getDateString.test.ts
+++ src/helpers/__tests__/getDateString.test.ts
@@ -11,7 +11,33 @@ describe('helpers/getDate', () => {
 		expect(dateString).toBe([
 			date.getUTCFullYear(),
 			`${(date.getUTCMonth() + 1)}`.padStart(2, '0'),
-			date.getUTCDate(),
+			`${(date.getUTCDate())}`.padStart(2, '0'),
+		].join('-'));
+	});
+
+	test('success - date diff +', () => {
+		const dateDiff = 100;
+		const date = faker.date.recent();
+		const dateString = getDateString(date, dateDiff);
+		const nextDate = new Date(date);
+		nextDate.setDate(nextDate.getDate() + dateDiff);
+		expect(dateString).toBe([
+			nextDate.getUTCFullYear(),
+			`${(nextDate.getUTCMonth() + 1)}`.padStart(2, '0'),
+			`${(nextDate.getUTCDate())}`.padStart(2, '0'),
+		].join('-'));
+	});
+
+	test('success - date diff -', () => {
+		const dateDiff = -100;
+		const date = faker.date.recent();
+		const dateString = getDateString(date, dateDiff);
+		const nextDate = new Date(date);
+		nextDate.setDate(nextDate.getDate() + dateDiff);
+		expect(dateString).toBe([
+			nextDate.getUTCFullYear(),
+			`${(nextDate.getUTCMonth() + 1)}`.padStart(2, '0'),
+			`${(nextDate.getUTCDate())}`.padStart(2, '0'),
 		].join('-'));
 	});
 });
--- src/helpers/getDateString.ts
+++ src/helpers/getDateString.ts
@@ -1,3 +1,7 @@
-export function getDateString(date: Date): string {
-	return date.toISOString().substr(0, 10);
+export function getDateString(date: Date, dateDiff?: number): string {
+	const nextDate = new Date(date);
+	if (dateDiff !== undefined) {
+		nextDate.setDate(nextDate.getDate() + dateDiff);
+	}
+	return nextDate.toISOString().substr(0, 10);
 }
