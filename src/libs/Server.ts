e8b65140513f342d80524143b1761b4742f01ffb
sapphire <info@sapphire.sh>
2019-10-19 21:11:14 +0900

    change local server port

--- src/libs/Server.ts
+++ src/libs/Server.ts
@@ -29,7 +29,7 @@ export class Server {
 			res.json(true);
 		});
 
-		this.server = this.app.listen(80);
+		this.server = this.app.listen(5101);
 	}
 
 	protected async getItems(): Promise<Item[]> {
