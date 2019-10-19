58c8a0a14f5ba7721868ff06e03ed00c33fa21b8
sapphire <info@sapphire.sh>
2019-10-19 13:03:39 +0900

    add composeTweet test

--- src/helpers/__tests__/composeTweet.test.ts
+++ src/helpers/__tests__/composeTweet.test.ts
@@ -1,11 +1,32 @@
-import faker from 'faker';
+import {
+	Item,
+} from '~/models';
 
 import {
 	composeTweet,
 } from '../composeTweet';
 
 describe('helpers/composeTweet', () => {
-	test('success', () => {
+	const item: Item = {
+		id: '2b0265a8f0dc02c97e754b2ea22c1f63cecd98f227eaeaf17ed258610ac557e8',
+		date: '2016-09-28',
+		title: 'PS4_Kitchen(키친)',
+		applicant: '게임피아(주)',
+		rating: 3,
+		platform: '03',
+		code: 'CC-NV-160928-001',
+		tweet: 0,
+	};
 
+	test('success', () => {
+		expect(composeTweet(item)).toBe([
+			'[2016-09-28]',
+			'[비디오 게임]',
+			'[PS4_Kitchen(키친)]',
+			'[게임피아(주)]',
+			'[청소년 이용불가]',
+			'https://www.grac.or.kr/Statistics/Popup/Pop_ReasonInfo.aspx?2b0265a8f0dc02c97e754b2ea22c1f63cecd98f227eaeaf17ed258610ac557e8',
+			'@GameRatingInfo',
+		].join('\n'));
 	});
 });
